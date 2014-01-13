b24e057dba63d16387b22b8d2a42326b6a61427c
chiky <chikyd.14@gmail.com>
2014-01-12 18:57:19 -0600

    Usuario CRUD

--- index.php
+++ index.php
@@ -13,6 +13,72 @@
 
 	$varchiky = $oPersona->checkToken();
 
-	var_dump($varchiky);
+	
+
+	/* 
+        ----------------------
+                 USUARIO -> CRUD
+        ----------------------
+*/
+
+        //Guarda Nuevo Usuario
+        if ( $option == 'nUsuario' ) {
+                //parametros
+                $nombre                   = $_POST['name'];
+                $apellido                 = $_POST['apellido'];
+                $user                         = $_POST['user'];
+            $password                 = $_POST['password'];
+            $estado                   = $_POST['estado'];
+            $id_privilegio        = $_POST['id_privilegio'];
+                //$password  = base64_encode($usuario->encriptar($password));
+                
+                $params = array($nombre, $apellido, $user, $password, $estado, $id_privilegio);
+                $save   = $usuario->nuevo($params);
+                if ( $save ) {
+                        echo "done";
+                } else {
+                        echo "error";
+                }
+        }
+
+        // Modificar usuario
+        if ( $option == 'mUsuario' ) 
+        {
+                //parametros
+                 
+                $nombre                   = $_POST['name'];
+                $apellido                 = $_POST['apellido'];
+                $user                         = $_POST['user'];
+            $password                 = $_POST['password'];
+            $estado                   = $_POST['estado'];
+            $id_privilegio        = $_POST['id_privilegio'];
+            $id_usuario                = $_POST['id_usuario'];
+                //$password  = base64_encode($usuario->encriptar($password));
+                
+                $params = array($nombre, $apellido, $user, $password, $estado, $id_privilegio, $id_usuario);
+                $update = $usuario->modificar($params);
+                if ( $update ) {
+                        echo "done";
+                } else {
+                        echo "error";
+                } 
+        }
+
+        //Eliminar Usuario
+        if ( $option == 'eUsuario' ) {
+            //parametros
+            $idUsuario = $_GET['id_usuario'];
+            $delete    = $usuario->eliminar($idUsuario);
+            if ( $delete ) {
+                echo "done";
+            } else {
+                echo "error";
+            }
+                }
+
+
+
+
+
 
 ?>
